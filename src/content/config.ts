2b1b9c0701ec929340e055f6b1fd90ea91c11503
brunoblaise1 <boogier94@gmail.com>
2025-01-12 18:38:26 +0200

    Helo

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,19 @@
 import { defineCollection, z } from "astro:content"
-
-
-const blogsCollection = defineCollection({
+const Collection = defineCollection({
     schema: z.object({
         title: z.string(),
         published: z.string(),
         language: z.string()
-
- 
     })
 })
 
+// const yearsCollection = defineCollection({
+//     schema: z.object({
+        
+//     })
+// })
 
-export const cllections = {
-    blogs: blogsCollection,
+export const collections = {
+    blogs: Collection,
+    years: Collection,
 }

c9cd2e61081e8b4c35ff02c93ec77e9256284ac9
brunoblaise1 <boogier94@gmail.com>
2025-04-12 12:22:28 +0200

    2025

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,18 @@
-import { defineCollection, z } from "astro:content"
+import { defineCollection, z } from "astro:content";
 const Collection = defineCollection({
     schema: z.object({
         title: z.string(),
         published: z.string(),
-        language: z.string()
-    })
-})
+        language: z.string(),
+    }),
+});
 
 // const yearsCollection = defineCollection({
 //     schema: z.object({
-        
+
 //     })
 // })
 
 export const collections = {
     blogs: Collection,
-    years: Collection,
-}
+};
